3494795dae19024740eb5b5ceaa54b7acce2e60a
ryanedmonds2000 <redmonds@hmc.edu>
2024-01-18 04:35:23 -0500

    add tests

---
+++ my_autograder/full_autograder.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+python3 run_tests.py
+
+ID=$(jq '.users[0].id' /autograder/submission_metadata.json)
+
+TIMESTAMP=$(jq '.created_at[:-6]' /autograder/submission_metadata.json)
+
+
+rm -r /autograder/${ID}
+cp -R /autograder/submission/ /autograder/${ID}
+
+echo ${TIMESTAMP} > /autograder/lemonade_grading/${ID}/timestamp.txt
+
+cd /autograder/lemonade_grading
+git add /autograder/lemonade_grading/${ID}
+git commit -m ${TIMESTAMP}
+git push
