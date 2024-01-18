2c6141a92417ab41e1e832f184891f3ec6d69cbb
ryanedmonds2000 <redmonds@hmc.edu>
2024-01-18 04:44:51 -0500

    fix zip command

--- my_autograder/make_autograder.sh
+++ my_autograder/make_autograder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # requires private key 'deploy_key' to give autograder git permissions
-zip -r autograder.zip setup.sh ssh_config deploy_key run_autograder full_autograder run_tests.py requirements.txt tests/*.py
+zip -r autograder.zip setup.sh ssh_config deploy_key run_autograder full_autograder.sh run_tests.py requirements.txt tests/*.py
