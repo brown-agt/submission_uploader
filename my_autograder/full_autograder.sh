45d3f2a923244df558c9caacc207b194f6f0b2d8
ryanedmonds2000 <redmonds@hmc.edu>
2024-01-22 16:29:07 -0500

    change references to lemonade_grading

--- my_autograder/full_autograder.sh
+++ my_autograder/full_autograder.sh
@@ -7,11 +7,11 @@ ID=$(jq '.users[0].id' /autograder/submission_metadata.json)
 TIMESTAMP=$(jq '.created_at[:-6]' /autograder/submission_metadata.json)
 
 rm -r /autograder/${ID}
-cp -R /autograder/submission/ /autograder/lemonade_grading/${ID}
+cp -R /autograder/submission/ /autograder/grading/${ID}
 
-echo ${TIMESTAMP} > /autograder/lemonade_grading/${ID}/timestamp.txt
+echo ${TIMESTAMP} > /autograder/grading/${ID}/timestamp.txt
 
-cd /autograder/lemonade_grading
-git add /autograder/lemonade_grading/${ID}
+cd /autograder/grading
+git add /autograder/grading/${ID}
 git commit -m ${TIMESTAMP}
 git push
